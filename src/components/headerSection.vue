6e06b88ad7aec7133f5d63056c945a16ff5ecef2
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-02-09 11:09:59 +0100

    header + prima parte main

--- src/components/headerSection.vue
+++ src/components/headerSection.vue
@@ -1,7 +1,10 @@
 <template>
 <div class="header-Section"> 
     <div class="header-info d-flex justify-content-between">
-            <span>Open Hours Mon - Sat - 0.00 - 18.00</span>
+            <div>
+                <i class="fa-solid fa-clock"></i>
+                <span>Open Hours Mon - Sat - 0.00 - 18.00</span>
+            </div>
             <div class="header-contact d-flex">
             <span class="px-2" >+1 (305) 1234 5678</span>
             <span class="px-2">hello@axample.com</span>
@@ -10,19 +13,26 @@
     <header>
           
         <nav class="d-flex justify-content-between py-3">
-        <span>NEXGEN</span>
+        <div class="title">
+            <span class="nex">NEX</span>
+            <span class="gen">GEN</span>
+        </div>
             <ul>
-                <li class="px-2" v-for="nav in header" :key="nav.id">{{nav.label}}</li>
-                <li class=" get-in-touch">GET IN TOUCH</li>
+                <li class="px-4 py-2" v-for="nav in header" :key="nav.id">{{nav.label}}</li>
+                <li class=" get-in-touch px-4 py-2">GET IN TOUCH</li>
             </ul>
         </nav>
         <div class="header-content d-flex flex-column justify-content-center">
-            <div>HUMAN RESOURCES</div>
-            <h1>Get More Productivity</h1>
-            <p>Planning, recruiment and selection process and performance evaluation of employees</p>
-            <div class="header-button">
-                <button>GET IN TOUCH</button>
-                <button> READ MORE</button>
+            <div class="h-res">HUMAN RESOURCES</div>
+            <span class="">
+                <p class="get-more">Get More</p>
+                <span class="productivity">Productivity</span>
+                 
+            </span>
+            <div class="subtitle">Planning, recruiment and selection process and performance evaluation of employees</div>
+            <div class="header-button d-flex my-4">
+                <div class="get-in-touch px-4 py-2">GET IN TOUCH</div>
+                <div class="read-more px-4 py-2 mx-3"> READ MORE</div>
             </div>
         </div>
     </header>
--- src/components/mainFirst.vue
+++ src/components/mainFirst.vue
@@ -1,9 +1,35 @@
 <template>
 <div class="main-first">
     <div class="three-box-container d-flex justify-content-between align-items-center">
-        <div class="card" v-for="card in threeCard" :key="card.id">
+        <div class="card px-4 d-flex flex-column justify-content-center" v-for="card in threeCard" :key="card.id">
             <h3>{{card.title}}</h3>
-            <p>{{card.text}}</p>
+            <p>{{card.text}}
+            </p>
+        </div>
+    </div>
+
+    <div class="the-company d-flex">
+        <div class="text d-flex flex-column justify-content-center ">
+            <strong class="about">ABOUT THE NETWORK</strong>
+            <strong class="title-section my-3">
+                <span class="the px-2">The</span>
+                <span class="px-2">Company</span>
+            </strong>
+            <p class="subtitle">Lorem ipsum dolor sit amet consectetur numquam  recusandae quasi ex dolor eos. Perspiciatis dolores asperiores dolor cum.</p>
+            <div class="block-container d-flex justify-content-between pt-3 ">
+                <div class="block d-flex flex-column pb-3" v-for="block in threeCardB" :key="block.id">
+                    <strong class="block-counter px-3 py-1">{{block.counter}}</strong>
+                    <h4>{{block.category}}</h4>
+                    <span class="block-text">{{block.text}}</span>
+                </div>
+            </div>
+            <div class="button-container d-flex pt-5">
+                <button class="get-in-touch py-3 px-5 mx-2">GET IN TOUCH</button>
+                <button class="read-more py-3 px-5 mx-2">READ MORE</button>
+            </div>
+        </div>
+        <div class="img">
+            <img src="@/assets/about-5.jpg" alt="">
         </div>
     </div>
 </div>
@@ -34,7 +60,30 @@ export default {
                     title: 'Performance',
                     text: 'Archieving maximum impact and value from investments in finance and supply chain.',
                 },
-            ]
+            ],
+            threeCardB:[
+                {
+                    id: '1',
+                    counter: '0',
+                    category: 'Employees',
+                    text: 'lorem ipsum dolor sit amet',
+                },
+
+                {
+                    id: '2',
+                    counter: '0',
+                    category: 'Partners',
+                    text: 'Lorem, ipsum dolor sit',
+                },
+
+                {
+                    id: '3',
+                    counter: '0',
+                    category: 'Customers',
+                    text: 'Lorem, ipsum dolor sit',
+                },
+            ],
+            
         }
     }
 }
